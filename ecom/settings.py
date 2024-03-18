35b5af3b9fd904c9d39a07c8a0a57a147c37f2a1
AYAZ KHAN <ayazbadsha786@gmail.com>
2024-03-18 18:58:59 +0500

    first commit

--- ecom/settings.py
+++ ecom/settings.py
@@ -133,9 +133,3 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rzxgraphics@gmail.com'
-EMAIL_HOST_PASSWORD = "cxtp bght dzye zomd"
